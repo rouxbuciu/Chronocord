a9350c06d1b8b97156df8c7ce4fb9fc264b6b328
roux george buciu <roux@acmelabs.ca>
2018-01-20 08:55:06 -0500

    Add colour definitions, activity definitions

--- Chronocord/Categories/Constants.swift
+++ Chronocord/Categories/Constants.swift
@@ -18,8 +18,24 @@ struct Constants {
         }
     }
     
-    
+
     struct ActivityNames {
+        static let chores                   = NSLocalizedString("Chores", comment: "")
+        static let downtime                 = NSLocalizedString("Downtime", comment: "")
+        static let exercise                 = NSLocalizedString("Exercise", comment: "")
+        static let family                   = NSLocalizedString("Family", comment: "")
+        static let food                     = NSLocalizedString("Food", comment: "")
+        static let healthcare               = NSLocalizedString("Healthcare", comment: "")
+        static let hobby                    = NSLocalizedString("Hobby", comment: "")
+        static let learning                 = NSLocalizedString("Learning", comment: "")
+        static let play                     = NSLocalizedString("Play", comment: "")
+        static let productive               = NSLocalizedString("Productive", comment: "")
+        static let romantic                 = NSLocalizedString("Romantic", comment: "")
+        static let shopping                 = NSLocalizedString("Shopping", comment: "")
         static let sleep                    = NSLocalizedString("Sleep", comment: "")
+        static let social                   = NSLocalizedString("Social", comment: "")
+        static let travel                   = NSLocalizedString("Travel", comment: "")
+        static let wasted                   = NSLocalizedString("Wasted", comment: "")
+        static let work                     = NSLocalizedString("Work", comment: "")
     }
 }
--- Chronocord/Model/Activity.swift
+++ Chronocord/Model/Activity.swift
@@ -9,18 +9,46 @@
 import Foundation
 import UIKit
 
+enum ActivityCategory {
+    case Chores
+    case Downtime
+    case Exercise
+    case Family
+    case Food
+    case Healthcare
+    case Hobby
+    case Learning
+    case Play
+    case Productive
+    case Romantic
+    case Shopping
+    case Sleep
+    case Social
+    case Travel
+    case Wasted
+    case Work
+}
+
 struct DefaultActivities {
-    
+    static let chores           = Activity(name: Constants.ActivityNames.chores,
+                                           colour: StandardColours.chores,
+                                           category: .Chores)
+    static let downtime         = Activity(name: Constants.ActivityNames.downtime,
+                                           colour: StandardColours.downtime,
+                                           category: .Downtime)
     static let sleep            = Activity(name: Constants.ActivityNames.sleep,
-                                           colour: StandardColours.grey)
+                                           colour: StandardColours.sleep,
+                                           category: .Sleep)
 }
 
 struct Activity {
     var name:                   String
     var colour:                 UIColor
+    var category:               ActivityCategory
     
-    init(name: String, colour: UIColor) {
+    init(name: String, colour: UIColor, category: ActivityCategory) {
         self.name               = name
         self.colour             = colour
+        self.category           = category
     }
 }
