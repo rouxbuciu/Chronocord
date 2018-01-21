ad1a2603a5f65d83924e1a736441e131cef899f2
roux george buciu <roux@acmelabs.ca>
2018-01-20 19:20:33 -0500

    Modify JSON layout

--- Chronocord/Categories/JSONConverter.swift
+++ Chronocord/Categories/JSONConverter.swift
@@ -12,22 +12,14 @@ import Foundation
 //
 //{
 //    "userID": {
-//        "dayData": {
-//            "2018-20-01" : {
-//                "weight": 213,
-//                "calories in": 2139,
-//                "calories out": 2309,
-//                "day quality": 4,
-//                "timeChunkID": "1239876192goeuha882m23823bx2"
-//            },
-//        },
-//        "timeChunks": {
-//            "chunkID": {
-//                "1239876192goeuha882m23823bx2": {
-//                    "timechunk info": info
-//                }
-//            }
+//        "2018-20-01" : {
+//            "weight": 213,
+//            "calories in": 2139,
+//            "calories out": 2309,
+//            "day quality": 4,
+//            "timeChunkID": {.....}
 //        }
-//    }
+//    },
+//    "anotherUserID": {...}
 //}
 
